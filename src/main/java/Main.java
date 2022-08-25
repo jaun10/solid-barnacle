cc6c9b86fa0bf813a67cc2372ba98deeffddb8de
jaun10 <112098751+jaun10@users.noreply.github.com>
2022-08-25 11:34:26 -0500

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -14,7 +14,7 @@ public class Main {
     
     @RequestMapping("/prueba")
     String home2() {
-        return "Hello World - v4!";
+        return "juanguarin";
     }
 
     public static void main(String[] args) throws Exception {
